2f6833b4fa30d21aa752ed7d58cfe86616bbe16a
sleleu <leleu.sebastien@hotmail.fr>
2024-10-16 14:54:01 +0200

    add error handling at create_sets()

--- srcs/split_dataset.py
+++ srcs/split_dataset.py
@@ -1,24 +1,31 @@
-from utils import load
+from utils import load, YELLOW, END
 import pandas as pd
 
 def split_features(df: pd.DataFrame, split_value: int)-> tuple[pd.DataFrame]:
     # Drop les labels du df d'origine et créer un dataset X_train X_test
     df = df.drop(columns=[0]) # Retirer les IDs
     df.columns = range(df.shape[1]) # Réarranger indexs des colonnes
-
     X_train = df.iloc[: split_value, :]
     X_test = df.iloc[split_value :, :]
     return (X_train, X_test)
 
 def split_labels(y: pd.DataFrame, split_value: int)-> tuple[pd.DataFrame]:
+    """Take a DataFrame of labels, and split into 2 parts, separated at 'split_value'"""
     y_train = y.iloc[: split_value]
     y_test = y.iloc[split_value :]
     return (y_train, y_test)
 
-def create_sets(sets : tuple[pd.DataFrame])-> None:
-    names = ["X_train.csv", "X_test.csv", "y_train.csv", "y_test.csv"]
-    for i, set in enumerate(sets):
-        set.to_csv(f"data/{names[i]}", index=False)
+def create_sets(sets: tuple[pd.DataFrame], names: list[str])-> None:
+    """Convert a tuple of dataframes in csv files"""
+    HANDLED_ERRORS = (FileNotFoundError, PermissionError,
+                      ValueError, OSError)
+    try: 
+        for i, set in enumerate(sets):
+            set.to_csv(f"data/{names[i]}", index=False)
+    except HANDLED_ERRORS as error:
+        print(f"{YELLOW}{__name__}: {type(error).__name__}: {error}{END}")
+        return exit(1)
+
 
 def split_dataset(dataset_name: str)-> None:
     df = load(dataset_name, header=None)
@@ -32,4 +39,5 @@ def split_dataset(dataset_name: str)-> None:
 
     X_train, X_test = split_features(df, split_value)
     y_train, y_test = split_labels(y, split_value)
-    create_sets((X_train, X_test, y_train, y_test))
+    names = ["X_train.csv", "X_test.csv", "y_train.csv", "y_test.csv"]
+    create_sets((X_train, X_test, y_train, y_test), names)
