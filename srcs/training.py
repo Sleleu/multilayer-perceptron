68ca4dfc40c09cfbee02f28bd63671bd1f2b0455
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2024-10-17 12:11:29 +0200

    add fed_forward

--- srcs/training.py
+++ srcs/training.py
@@ -8,10 +8,18 @@ def init_weights(output_size, input_size):
     biases = np.random.rand(output_size, 1)
     return weights, biases
 
-"""
-def feed_forward(X_train, y_train, W, b):
 
-"""
+def feed_forward(X_train, W, b):
+    A = [X_train.T]
+    Z = []
+
+    for c in range(len(W)):
+        z = np.dot(W[c], A[c]) + b[c] # Z = W.A + b
+        a = sigmoid(z)
+        Z.append(z)
+        A.append(a)
+    
+    return A, Z
 
 def training(dataset: str):
     X_train = load("data/X_train.csv", header=None)
@@ -30,10 +38,7 @@ def training(dataset: str):
         W.append(weights)
         b.append(biases)
     
-    print(W[0].shape)
-    print(W[1].shape)
-
-    print(b[0].shape)
-    print(b[1].shape)
-    #feed_forward(X_train, y_train, W, b)
+    A, Z = feed_forward(X_train, W, b)
+    print(f"A: {A}")
+    print(f"Z: {Z[0]}")
 
